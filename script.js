e9b8e4d36bf43c20b817d4b8d73beb3a6568b04c
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-22 13:36:47 +0000

    6-04

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let abase = 0
 let bbase = 0
 let hheight = 0
+let sum = 0
+let p = 0
 
-document.getElementById('button').addEventListener('click', multiplication)
+document.getElementById('answer').addEventListener('click', calculation)
 
-function multiplication () {
-  number2 = document.getElementById('input2').value
-  number = document.getElementById('input').value
-  number2 = parseInt(number2)
-  number = parseInt(number)
-  answer = number * number2
-  document.getElementById('answer').innerHTML = answer
+function calculation () {
+  bbase = document.getElementById('b-base').value
+  abase = document.getElementById('a-base').value
+  hheight = document.getElementById('h-height').value
+  bbase = parseInt(bbase)
+  abase = parseInt(abase)
+	hheight = parseInt(hheight)
+  sum = (abase + bbase) / 2
+  p = sum * hheight
+  document.getElementById('paragraph').innerHTML = p
 }
